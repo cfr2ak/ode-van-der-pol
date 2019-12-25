34705af18d9ab7f2ccd92d22c04dcc73f7776bd9
cfr2ak <cfr2ak@gmail.com>
2019-12-25 10:54:41 +0900

    Add basic ode struct

---
+++ src/main.cxx
@@ -0,0 +1,25 @@
+#include <vector>
+
+
+using namespace std;
+
+
+template<class Stepper>
+struct Odeint
+{
+  static const int max_step = 50000;
+  double eps;
+  int nok;
+  int nbed;
+  int nvar;
+  double x1, x2, hmin;
+  bool dense;
+  vector<double> y, dydx;
+  vector<double> &ystart;
+  Output &out;
+  typename Stepper::Dtype &derives;
+  Stepper s;
+  int nstp;
+  double x, h;
+  Odeint(vector<double>
+  
